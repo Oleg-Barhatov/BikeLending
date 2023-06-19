9fe4d11c422b57c6eae983e565cac096ff50031a
Oleg Barhatov <oleg-barhatov@yandex.ru>
2023-06-19 17:42:32 +0300

    del for SCSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,2 @@
 import './index.css';
 
-const burgerMenu = document.querySelector('.header-mobile')
-
-burgerMenu.addEventListener('click', (e) => {
-  e.target.classList.toggle('header-mobile_open')
-})
