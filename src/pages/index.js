1528f9290be749756b75f062f625d56ba5f0b1fa
Oleg Barhatov <oleg-barhatov@yandex.ru>
2023-06-19 17:41:14 +0300

    del

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,7 @@
 import './index.css';
+
+const burgerMenu = document.querySelector('.header-mobile')
+
+burgerMenu.addEventListener('click', (e) => {
+  e.target.classList.toggle('header-mobile_open')
+})
