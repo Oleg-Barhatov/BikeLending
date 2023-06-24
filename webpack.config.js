937e4367747de11a8037c123f298d78f0855a3d4
Oleg Barhatov <oleg-barhatov@yandex.ru>
2023-06-24 13:13:46 +0300

    errors fonts

--- webpack.config.js
+++ webpack.config.js
@@ -25,17 +25,26 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        use: ["file-loader"],
         type: 'asset/resource',
       },
       {
-        test: /\.(scss|css)$/,
+        test: /\.(s[ac]ss)$/i,
         use: [MiniCssExtractPlugin.loader, {
             loader: 'css-loader',
             options: {
               importLoaders: 1
             }
           },
-          'postcss-loader', 
+          'postcss-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                includePaths: ["src"]
+              }
+            }
+          }
         ]
       },
     ]
